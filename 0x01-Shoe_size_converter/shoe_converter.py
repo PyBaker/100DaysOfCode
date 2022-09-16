530c707048f2a91da1a8bc7b488b20234be6c919
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-09-16 11:49:30 +0200

    added shoe converter

--- 0x01-Shoe_size_converter/shoe_converter.py
+++ 0x01-Shoe_size_converter/shoe_converter.py
@@ -1,33 +1,41 @@
+from ctypes import sizeof
 import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 
 
-count = 0
+uk_size = 0
 
 def clicked(answer=""): # without event because I use `command=` instead of `bind`
-    global count
+    global uk_size
 
     if answer == 'plus':
-        count = count + 1
+        uk_size = uk_size + 1
     elif answer == 'minus':
-        count = count - 1
+        uk_size = uk_size - 1
 
-    label1.configure(text=f'Size in US => {count}')
+    us_size = uk_size + 1
+
+    label1.configure(text=f'{uk_size} Size in US => {us_size}')
 
 
 windows = tk.Tk()
+windows.geometry('360x260')
 windows.title("Shoe Size Converter by PyBaker")
+
+Grid.rowconfigure(windows,0,weight=3)
+Grid.columnconfigure(windows,0,weight=1)
 
 label = tk.Label(windows, text="Click on the Button")
 label.grid(column=0, row=0)
 
 label1 = tk.Label(windows)
-label1.grid(column=0, row=1)
+label1.grid(column=0, row=2)
 
 custom_button2 = ttk.Button(windows, text="Minus (-) UK size", command=lambda: clicked('minus'))
-custom_button2.grid(column=1, row=0, pady=10, padx=10)
+custom_button2.grid(column=0, row=1, sticky="NSEW", pady=40, padx=40)
 
 custom_button = ttk.Button(windows, text="Add (+) UK size", command=lambda: clicked('plus'))
-custom_button.grid(column=2, row=0, pady=10, padx=10)
+custom_button.grid(column=0, row=3, sticky="NSEW",  pady=40, padx=40)
 
 windows.mainloop()
